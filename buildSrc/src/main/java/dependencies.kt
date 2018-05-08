3a10693a8c1a360f2f3a6a4852f080693d7347ad
IVIanuu <IVIanuu@gmail.com>
2018-05-08 15:30:18 +0200

    Fixed build

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -9,19 +9,21 @@ object Versions {
     const val versionName = "1.0"
 
     const val kotlin = "1.2.41"
-    const val maven = "2.0"
-    const val rxContentObserver = "8e272960cf"
+    const val mavenGradle = "2.1"
+
+    const val rxContentObserver = "6d7e129598"
     const val rxJava = "2.1.13"
+
     const val support = "27.1.1"
 }
-
+//
 object Deps {
     const val androidGradlePlugin = "com.android.tools.build:gradle:3.1.2"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jre7:${Versions.kotlin}"
 
-    const val mavenGradlePlugin = "com.github.dcendents:android-maven-gradle-plugin:${Versions.maven}"
+    const val mavenGradlePlugin = "com.github.dcendents:android-maven-gradle-plugin:${Versions.mavenGradle}"
 
     const val supportAppCompat = "com.android.support:appcompat-v7:${Versions.support}"
 
