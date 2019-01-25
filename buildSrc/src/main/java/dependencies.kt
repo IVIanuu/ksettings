e0e066c63f865ab911fa5528297fbce87c019d58
IVIanuu <IVIanuu@gmail.com>
2019-01-25 16:02:20 +0100

    Gradle changes

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -13,7 +13,7 @@ object Build {
 
 object Versions {
     const val androidGradlePlugin = "3.3.0"
-    const val androidxAppCompat = "1.0.0"
+    const val androidxAppCompat = "1.0.2"
     const val androidxLifecycle = "2.0.0"
     const val coroutines = "1.0.0"
     const val kotlin = "1.3.20"
